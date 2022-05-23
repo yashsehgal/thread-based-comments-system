caa8dc3281177f9a3d83e9a50a3b172c7e6bb47c
Yash Sehgal <yashsehgal.work@gmail.com>
2022-05-23 16:09:04 +0000

    chore: interface frontend logic created

--- src/CommentWidget.tsx
+++ src/CommentWidget.tsx
@@ -11,7 +11,8 @@ interface Comment {
 };
 
 export default function CommentWidget() {
-    const [commentThreadRef, setCommentThread] = useState(null);
+    const [commentThreadRef, setCommentThread] = useState([]);
+    const [threadInputVisibilityRef, setThreadInputVisibility] = useState('none');
 
     let postCaption: PostCaption = {
         caption: 'This is my first ever photo on this platform, this is so cool :)'
@@ -20,8 +21,38 @@ export default function CommentWidget() {
     return (
         <React.Fragment>
             <div className='comment-widget-wrapper p-4 h-fit border border-dashed border-gray-500 rounded-md bg-white'>
-                <div className="post-caption-wrapper">
-                    <p className='leading-snug text-sm text-gray-800 font-normal'>{postCaption.caption}</p>
+                <div className="post-caption-wrapper w-full h-auto flex flex-row items-center justify-between">
+                    <p className='leading-snug font-semibold text-gray-800'>{postCaption.caption}</p>
+                    <button className='px-4 py-1 flex flex-row items-center justify-center text-white font-semibold rounded-md bg-blue-600 hover:bg-blue-800'
+                        onClick={() => {
+                            if (threadInputVisibilityRef.toLowerCase() === "none") {
+                                setThreadInputVisibility("flex");
+                            }
+                        }}
+                    >
+                        Comment
+                    </button>
+                </div>
+                <div className='new-comment-thread-input-wrapper w-full h-auto flex flex-row items-center justify-between gap-8 mt-6'
+                    style={{
+                        display: threadInputVisibilityRef
+                    }}
+                >
+                    <input type="text" placeholder='Type your comment or response to this post :)' 
+                        className='px-4 py-1.5 rounded-md border border-gray-400 w-full'
+                    />
+                    <button className='px-4 py-1 flex flex-row items-center justify-center text-white font-semibold rounded-md bg-green-600 hover:bg-green-800'>
+                        Reply
+                    </button>
+                    <button className='px-4 py-1 flex flex-row items-center justify-center text-white font-semibold rounded-md bg-red-600 hover:bg-red-800'
+                        onClick={() => {
+                            if (threadInputVisibilityRef.toLowerCase() === "flex") {
+                                setThreadInputVisibility("none");
+                            }
+                        }}
+                    >
+                        Discard
+                    </button>
                 </div>
             </div>
         </React.Fragment>
